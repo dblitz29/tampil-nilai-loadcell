d2118a3dd6d1390710c71fb17c02b1a7bf5ef891
dblitz29 <primafadhil.sulistyo@gmail.com>
2022-07-18 19:24:06 +0700

    deksripsi

---
+++ lib/main.dart
@@ -0,0 +1,120 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:web_socket_channel/io.dart';
+import 'package:web_socket_channel/status.dart' as status;
+import 'temphumi.dart';
+
+const String esp_url = 'ws://192.168.4.1:81';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'DHT11 eps-websocket-flutter',
+      home: MyHomePage(title: 'Tes Suhu Realtime'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool isLoaded = false;
+  String msg = '';
+  TempHumi dht = TempHumi(0, 0);
+  final channel = IOWebSocketChannel.connect(esp_url);
+  @override
+  void initState() {
+    super.initState();
+
+    channel.stream.listen(
+      (message) {
+        channel.sink.add('Flutter received $message');
+        if (message == "connected") {
+          print('Received from MCU: $message');
+          setState(() {
+            msg = message;
+          });
+        } else {
+          print('Received from MCU: $message');
+          // {'tempC':'30.50','humi':'64.00'}
+          Map<String, dynamic> json = jsonDecode(message);
+          setState(() {
+            dht = TempHumi.fromJson(json);
+            isLoaded = true;
+          });
+        }
+        //channel.sink.close(status.goingAway);
+      },
+      onDone: () {
+        //if WebSocket is disconnected
+        print("Web socket is closed");
+        setState(() {
+          msg = 'disconnected';
+          isLoaded = false;
+        });
+      },
+      onError: (error) {
+        print(error.toString());
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+          child: !isLoaded
+              ? const CircularProgressIndicator()
+              : Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: [
+                        Text('Kalibrasi: ',
+                            style: TextStyle(fontSize: 50, color: Colors.blue)),
+                        Text(
+                          '${dht.tempC.toStringAsFixed(1)}',
+                          style: TextStyle(fontSize: 50, color: Colors.blue),
+                        ),
+                        Text(
+                          '%',
+                          style: TextStyle(fontSize: 30, color: Colors.blue),
+                        )
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text('Berat: ',
+                            style: TextStyle(fontSize: 50, color: Colors.blue)),
+                        Text(
+                          '${dht.humi.toStringAsFixed(1)}',
+                          style: TextStyle(fontSize: 50, color: Colors.blue),
+                        ),
+                        Text(
+                          ' gr',
+                          style: TextStyle(fontSize: 20, color: Colors.blue),
+                        )
+                      ],
+                    )
+                  ],
+                )),
+    );
+  }
+}
